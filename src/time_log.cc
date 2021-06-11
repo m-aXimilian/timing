f13be8defb06319274ca9b68d3c5d3d5dfad0b70
m-aXimilian <kueffnermax@gmail.com>
2021-06-11 19:09:26 +0200

    edit: removed a few debug messages

--- src/time_log.cc
+++ src/time_log.cc
@@ -17,7 +17,7 @@ tlog::TimeLog::TimeLog( std::shared_ptr<data::Database> db,
     time_table_ = time; 
 
     db_descriptor_->ConnectDatabase();
-    
+
 }
 
 
@@ -36,43 +36,36 @@ int tlog::TimeLog::LogIn(){
     db_descriptor_->SelectFromTable(tmp);
 
     size_t query_size{db_descriptor_->query_result_->size()};
-
-    if (query_size != 0){
-     
-        std::string last_action{db_descriptor_->query_result_->back()};
-
-        if (last_action == "I") return -1;
-    }
+    
+    if (query_size == 2 ||
+        last_action_ == tlog::action::LOG_IN) return -1;
     
     if (query_size == 0){
 
         std::vector<std::string> fields{
-            *db_descriptor_->table_columns_->get_as<std::string>("column_first"),
-            *db_descriptor_->table_columns_->get_as<std::string>("column_second"),
-            *db_descriptor_->table_columns_->get_as<std::string>("column_sixth")
+            "date", "login"
+            //*db_descriptor_->table_columns_->get_as<std::string>("column_first"),
+            //*db_descriptor_->table_columns_->get_as<std::string>("column_second")
         };
 
         std::vector<std::string> values{
             time_table_->date_raw_time().first,
-            time_table_->date_raw_time().second,
-            "I"
+            time_table_->date_raw_time().second
         };
 
         db_descriptor_->NewEntry(fields,values);
+
+        last_action_ = tlog::action::LOG_IN;
     
         return 0;
     }
-    
+
     UpdatePause(    time_table_->date_raw_time().second, 
                     db_descriptor_->query_result_->at(2),
                     db_descriptor_->query_result_->at(3) );
 
-    std::vector<std::string> fields{*db_descriptor_->table_columns_->get_as<std::string>("column_sixth")};
-
-    std::vector<std::string> values{"I"};
-
-    db_descriptor_->UpdateTable(fields, values, std::string("date='")
-            .append(time_table_->date_raw_time().first).append("\'"));
+    last_action_ = tlog::action::LOG_IN;
+    
     
     return 0;
 }
@@ -87,15 +80,14 @@ int tlog::TimeLog::LogOut(){
     
     db_descriptor_->SelectFromTable(tmp);
 
+    std::cout<<db_descriptor_->query_result_->at(0);
+
     size_t query_size{db_descriptor_->query_result_->size()};
 
-    if( query_size < 3 ){
-        std::string last_action{db_descriptor_->query_result_->back()};
-        
-        if (last_action == "O") return -1;
-    }
+    if( query_size < 2 ||
+        last_action_ == tlog::action::LOG_OUT) return -1;
     
-    if( query_size < 5 ){
+    if( query_size < 3 ){
         std::vector<std::string> fields{*db_descriptor_->table_columns_->get_as<std::string>("column_fourth")};
         
         std::vector<std::string> values{std::string("0")};
@@ -104,13 +96,9 @@ int tlog::TimeLog::LogOut(){
             .append(time_table_->date_raw_time().first).append("\'"));
     }
     
-    std::vector<std::string> fields{*db_descriptor_->table_columns_->get_as<std::string>("column_third"),
-                                    *db_descriptor_->table_columns_->get_as<std::string>("column_sixth")
-                                    };
+    std::vector<std::string> fields{*db_descriptor_->table_columns_->get_as<std::string>("column_third")};
 
-    std::vector<std::string> values{time_table_->date_raw_time().second, 
-                                    "O"
-                                    };
+    std::vector<std::string> values{time_table_->date_raw_time().second};
 
     db_descriptor_->UpdateTable(fields, values, std::string("date='")
             .append(time_table_->date_raw_time().first).append("\'"));
@@ -127,11 +115,11 @@ int tlog::TimeLog::LogOut(){
 
 int tlog::TimeLog::WriteWorkTime(std::vector<std::string> *query, std::time_t &last_logout){
 
-    if ( query->size() < 4 ) return -1;
+    if ( query->size() < 3 ) return -1;
 
     std::vector<std::time_t> times;
 
-    for (std::vector<std::string>::iterator it = query->begin(); it != query->end()-1; ++it){
+    for (std::vector<std::string>::iterator it = query->begin(); it != query->end(); ++it){
         times.push_back(time_table_->StringToTime(*it));
     }
 
